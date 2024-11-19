17f6a481c81fa4317c45ee44aa019d3c2b44b12f
Frida Lannerström <frida.lannerstrom@tqnordic.com>
2024-11-19 20:01:19 +0000

    feat: add new dogbreed objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("hello!")
+// set all dog breeds
+
+let dogBreeds = [
+    { name: "Labrador", size: "large", score: 0 },
+    { name: "Beagle", size: "medium", score: 0 },
+    { name: "Pug", size: "small", score: 0 },
+    { name: "Australian Shepherd", size: "medium", score: 0 },
+    { name: "Newfoundland", size: "extra large", score: 0 }
+];
