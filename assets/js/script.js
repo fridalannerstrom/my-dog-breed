43ea99a20283ffcbdcffd2922a5161c5bc904cf1
Frida Lannerström <frida.lannerstrom@tqnordic.com>
2024-11-20 08:46:57 +0000

    refactor: restructure and comment multi step form javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,54 @@ let dogBreeds = [
     }
 ];
 
+// Multi step form
+
+document.addEventListener("DOMContentLoaded", function () { // Wait for HTML document to load so all classes are available
+
+    // Get relevant HTML elements
+    let steps = document.querySelectorAll(".step"); // Gives a list over all steps
+    let nextButton = document.getElementById("next-button");
+    let prevButton = document.getElementById("prev-button");
+
+    // Get the first step
+    let currentStep = 0; 
+
+    // Show/hide step based on "currentStep", adding active class to current step
+    function showStep(stepIndex) { // parameter stepIndex gives the number of the current step
+        steps.forEach((step, index) => { // forEach goes through each step, index shows number for relevant step
+            if (index === stepIndex) { 
+                step.classList.add("active"); // adds active class to current step
+            } else {
+                step.classList.remove("active"); // removes active class from non current step
+            }
+        });
+
+    prevButton.style.display = stepIndex === 0 ? "none" : "inline-block"; // Hide prev button from first question
+    nextButton.textContent = stepIndex === steps.length - 1 ? "Submit" : "Next"; // Changes from "next" to "submit" on last step
+    }
+
+    // function for adding steps
+    nextButton.addEventListener("click", function () {
+        if (currentStep < steps.length - 1) { // checks if its not the last step
+            currentStep++; // Add 1 to currentStep
+            showStep(currentStep); // Show next step
+        } else {
+            alert("Form submitted!"); // CHANGE THIS TO A REDIRECT LATER
+        }
+    });
+
+    // function for going back steps
+    prevButton.addEventListener("click", function () {
+        if (currentStep > 0) { // checks if its not the first step
+            currentStep--; // removes 1 from currentStep
+            showStep(currentStep); // Show prev step
+        }
+    });
+
+    // Show first step
+    showStep(currentStep);
+});
+
 // Count score with quiz
 
     //Get the value
